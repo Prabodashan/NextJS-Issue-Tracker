68ba556b89435b741383111cedd4a8197e5bb958
Prabodashan <prabodashan14@gmail.com>
2024-09-10 18:53:55 +0530

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import { issueSchema } from "@/app/validationSchemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { z } from "zod";
-import IssueForm from "../_components/IssueForm";
+import IssueFormSkeleton from "./loading";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
+  loading: () => <IssueFormSkeleton />,
 });
-
-type IssueForm = z.infer<typeof issueSchema>;
 
 const NewIssuePage = () => {
   return <IssueForm />;
